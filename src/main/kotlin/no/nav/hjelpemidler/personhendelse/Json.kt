30033561107b7137930ac17f639853e5fbd4b75e
Erik Eiesland <erik.eiesland@nav.no>
2024-06-04 13:56:42 +0200

    trigger bygg

--- src/main/kotlin/no/nav/hjelpemidler/personhendelse/Json.kt
+++ src/main/kotlin/no/nav/hjelpemidler/personhendelse/Json.kt
@@ -5,8 +5,7 @@ import com.fasterxml.jackson.databind.json.JsonMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonMapperBuilder
 
-val jsonMapper: JsonMapper =
-    jacksonMapperBuilder()
-        .addModule(JavaTimeModule())
-        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-        .build()
+val jsonMapper: JsonMapper = jacksonMapperBuilder()
+    .addModule(JavaTimeModule())
+    .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+    .build()
